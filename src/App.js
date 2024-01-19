7f729b07fe6ce1aa6cf1dd5b9a2894212c7509f8
カランワル ハーシュ <karanwal-ha@MACN0068.local>
2024-01-19 15:22:11 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
+import './App.scss';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+  <section class="advice">
+    <h1 class="advice__title">Site under construction</h1>
+  </section>
+  <section class="city-stuff">
+    <ul class="skyscrappers__list">
+      <li class="skyscrapper__item skyscrapper-1"></li>
+      <li class="skyscrapper__item skyscrapper-2"></li>
+      <li class="skyscrapper__item skyscrapper-3"></li>
+      <li class="skyscrapper__item skyscrapper-4"></li>
+      <li class="skyscrapper__item skyscrapper-5"></li>
+    </ul>
+    <ul class="tree__container">
+      <li class="tree__list">
+        <ul class="tree__item tree-1">
+          <li class="tree__trunk"></li>
+          <li class="tree__leaves"></li>
+        </ul>
+        <ul class="tree__item tree-2">
+          <li class="tree__trunk"></li>
+          <li class="tree__leaves"></li>
+        </ul>
+        <ul class="tree__item tree-3">
+          <li class="tree__trunk"></li>
+          <li class="tree__leaves"></li>
+        </ul>  
+        <ul class="tree__item tree-4">
+          <li class="tree__trunk"></li>
+          <li class="tree__leaves"></li>
+        </ul>  
+        <ul class="tree__item tree-5">
+          <li class="tree__trunk"></li>
+          <li class="tree__leaves"></li>
+        </ul>  
+        <ul class="tree__item tree-6">
+          <li class="tree__trunk"></li>
+          <li class="tree__leaves"></li>
+        </ul>  
+        <ul class="tree__item tree-7">
+          <li class="tree__trunk"></li>
+          <li class="tree__leaves"></li>
+        </ul>  
+        <ul class="tree__item tree-8">
+          <li class="tree__trunk"></li>
+          <li class="tree__leaves"></li>
+        </ul>  
+      </li>
+    </ul>
+    <ul class="crane__list crane-1">
+      <li class="crane__item crane-cable crane-cable-1"></li>
+      <li class="crane__item crane-cable crane-cable-2"></li>
+      <li class="crane__item crane-cable crane-cable-3"></li>
+      <li class="crane__item crane-stand"></li>
+      <li class="crane__item crane-weight"></li>
+      <li class="crane__item crane-cabin"></li>
+      <li class="crane__item crane-arm"></li>
+    </ul>
+    <ul class="crane__list crane-2">
+      <li class="crane__item crane-cable crane-cable-1"></li>
+      <li class="crane__item crane-cable crane-cable-2"></li>
+      <li class="crane__item crane-cable crane-cable-3"></li>
+      <li class="crane__item crane-stand"></li>
+      <li class="crane__item crane-weight"></li>
+      <li class="crane__item crane-cabin"></li>
+      <li class="crane__item crane-arm"></li>
+    </ul>
+    <ul class="crane__list crane-3">
+      <li class="crane__item crane-cable crane-cable-1"></li>
+      <li class="crane__item crane-cable crane-cable-2"></li>
+      <li class="crane__item crane-cable crane-cable-3"></li>
+      <li class="crane__item crane-stand"></li>
+      <li class="crane__item crane-weight"></li>
+      <li class="crane__item crane-cabin"></li>
+      <li class="crane__item crane-arm"></li>
+    </ul>
+  </section>
+</main>
     </div>
   );
 }
